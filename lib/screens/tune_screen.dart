8b34a57451b79572484bcf1ece454dd705cb9556
faresarar <faresarar455@gmail.com>
2024-07-19 12:47:54 +0100

    Displaying tune items

--- lib/screens/tune_screen.dart
+++ lib/screens/tune_screen.dart
@@ -3,23 +3,32 @@ import 'package:tune_app/widgets/tune_item.dart';
 
 class TuneScreen extends StatelessWidget {
   const TuneScreen({super.key});
-
+  final List<Color> tuneColors = const [
+    Color(0xffF44336),
+    Color(0xffF89800),
+    Color(0xffFEEB3B),
+    Color(0xff4CAF50),
+    Color(0xff2896F3),
+    Color(0xff9C27B0),
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         elevation: 0, // to make appbar without shadows
-        backgroundColor: Color(0xff253238),
-        title: Text("Flutter Tune "),
+        backgroundColor: const Color(0xff253238),
+        title: const Text("Flutter Tune "),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          TuneItem(color: Colors.red),
-
-
-        ],
-      ) ,
+      body: Column(children: getTuneItems()),
     );
+  }
+
+  List<TuneItem> getTuneItems() {
+    List<TuneItem> items = [];
+    for (var color in tuneColors) {
+      items.add(TuneItem(color: color));
+    }
+    return items;
   }
 }
